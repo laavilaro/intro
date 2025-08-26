b9f995d541f32334646f059635cca05905e62a63
laavilaro <42155338+laavilaro@users.noreply.github.com>
2025-08-26 10:41:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Mi primera App!")
+st.title("Mi primera App 2025!")
 st.write("Easy")
 image = Image.opne('gato.jpg')
 st.image(image, caption = 'Un gato')
